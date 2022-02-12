3842fe16c74805a2c41f5a6ecbe5101fe1ca083f
Nichapat Komjit <oom-nichapat@hotmail.com>
2022-02-12 17:20:29 +0700

    update google map

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project/pages/map.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(MyApp());
